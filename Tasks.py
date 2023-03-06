2e5c99e38fedab65af438a53acef2e2232868c3a
Alex <best1972@list.ru>
2023-03-06 11:55:55 +0300

    made tasks 26

---
+++ Tasks.py
@@ -0,0 +1,18 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит 
+# число А в целую степень B с помощью рекурсии. Пример:  A = 3; B = 5 -> 243 (3⁵)       
+# A = 2; B = 3 -> 8 
+
+# number = int(input('Enter a number A: '))
+# degree = int(input('Enter a degree B: '))
+
+# def powering (number, degree):
+#     if degree == 0:
+#         return 1
+#     elif degree == 1:
+#         return number
+#     else:
+#         return number * powering(number, degree-1)
+
+# print(powering(number, degree))
+
+    
